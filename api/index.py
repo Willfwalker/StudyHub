f81b5cbeffbe630215a3aae51451ed62a9ef85ce
Willfwalker <105671605+Willfwalker@users.noreply.github.com>
2024-12-26 10:23:41 -0800

    vecel 3.0

--- api/index.py
+++ api/index.py
@@ -3,4 +3,6 @@ from app import app
 
 def handler(request):
     """Handle requests from Vercel serverless function"""
+    if request.method == "POST":
+        return app.handle_request(request)
     return app
